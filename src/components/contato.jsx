7a4d60235451aebbf0523023839475d006dc9dd2
thyezoliveira <thyezoliveira@gmail.com>
2025-11-23 11:28:08 -0300

    func(ui): Pagina de contato reorganizada e agicionado copyright.

--- src/components/contato.jsx
+++ src/components/contato.jsx
@@ -38,17 +38,20 @@ const SectionStyled = styled.section`
 
     div.division{
         border-bottom: dashed 1px #FF0;
+        margin: 16px 0;
     }
 
     div.socialHolder{
         display: flex;
-        justify-content: space-between;
+        justify-content: center;
+        margin: 32px 0;
 
         a{
             opacity: 0;
             font-size: 14px;
             transform: translateY(20px);
             span{
+                margin: 8px;
                 img{
                     width: 35px;
                 }
@@ -56,11 +59,49 @@ const SectionStyled = styled.section`
         }
     }
 
+    div.cpr{
+        position: relative;
+        bottom: -20px;
+        left: 0;
+        background-color: rgba(255, 255, 0, .8);
+        border: dashed 1px #FF0;
+        width: 100%;
+        height: 100px;
+        color: black;
+        overflow: hidden;
+        letter-spacing: 0.1%;
+
+        p{
+            color: black;
+            position: absolute;
+            top: -10px;
+            right: 10px;
+        }
+
+        span.c{
+            font-size: 200px;
+            font-weight: 900;
+            position: absolute;
+            top:0;
+            left: 0;
+            transform: translate(-32%, -30%);
+        }
+        
+        span.cp{
+            font-size: 65px;
+            font-weight: 400;
+            position: absolute;
+            top:0;
+            left: 0;
+            transform: translate(34%, 24%);
+        }
+    }
+
 `;
 
 const BtnCotacao = styled(CTAButton)`
     width: 100%;
-    margin: 32px 0;
+    margin: 4px 0;
 `;
 
 export default function ContactPage({setEstado}){
@@ -86,10 +127,8 @@ export default function ContactPage({setEstado}){
             <h1>CONTATO</h1>
             <h3>Vamos forjar algo incrível juntos?</h3>
             <p>Primeira conversa é gratuita. Sem compromisso, apenas foco em entender sua necessidade.</p>
-            {/* <BtnCotacao onClick={() => setEstado(3)} disabled>Peça seu orçamento</BtnCotacao> */}
-            <div ref={ReferenciaLINE1} className="division"></div>
 
-            <BtnCotacao onClick={() => setEstado(2)}>CV</BtnCotacao>
+            
 
             {/* <div ref={ReferenciaLINE2} className="division"></div> */}
 
@@ -97,6 +136,20 @@ export default function ContactPage({setEstado}){
                 <a ref={ReferenciaGIT} href="https://github.com/thyezoliveira" ><span><img src="/github_icone.svg" alt="" /></span></a>
                 <a ref={ReferenciaINSTA} href="https://www.instagram.com/forja_tech/" ><span><img src="/instagram_icone.svg" alt="" /></span></a>
                 <a ref={ReferenciaLINKEDIN} href="https://www.linkedin.com/in/engsofthyezoliveira/" ><span><img src="/linkedin_icone.svg" alt="" /></span></a>
+            </div>
+
+            {/* <div ref={ReferenciaLINE1} className="division"></div> */}
+
+            <BtnCotacao onClick={() => setEstado(2)}>Curriculum Vitae</BtnCotacao>
+            <BtnCotacao onClick={() => setEstado(3)}>Peça seu orçamento</BtnCotacao>
+
+            <div className="cpr">
+                <span className="c">©</span> 
+                <span className="cp">
+                Copyright
+
+                </span>
+                <p>forjatech | 2025</p>
             </div>
 
 
